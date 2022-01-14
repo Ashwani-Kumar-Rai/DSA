7da6ceaad48cda1e11515761de38a9513b30daa3
Ashwani-Kumar-Rai <ashwani.profession@gmail.com>
2022-01-14 10:07:43 +0530

    Arrays and String

---
+++ array_Basics/1_Find Smallest Element in an Array.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int size;
+    cout << "Enter size of array : " << endl;
+    cin >> size;
+    int arr[size];
+    cout << "Enter elements of array : " << endl;
+    for(int i=0; i<size; i++)
+    {
+        cin >> arr[i];
+    }
+    int smallest = arr[0];   // initialize smallest to arr[0]
+    for(int i=1; i<size; i++)  
+    {
+        if(arr[i]<smallest)   // compare current element with smallest
+        {
+            smallest = arr[i];
+        }
+    }
+    cout << "Smallest Element in the array : " << endl << smallest;  // display result
+    return 0;
+}
